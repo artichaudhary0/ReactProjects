572adce53be6606da8a8df2ebd49ee4ec368859c
artichaudhary0 <chaudhary.aarti1998@gmail.com>
2025-02-03 17:05:29 +0530

    React-Project

---
+++ propsandstate/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ToDoApp from "./components/Todo";
+
+function App() {
+  return (
+    <>
+      <ToDoApp />
+    </>
+  );
+}
+
+export default App;
